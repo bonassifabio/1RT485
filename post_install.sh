2f396533149c528063a1418db128f92d7a2ea7d7
Fabio Bonassi <bonassi.fabio.94@gmail.com>
2025-02-05 14:40:58 +0100

    Add custom functions to path when building the mybinder environment

--- post_install.sh
+++ post_install.sh
@@ -8,3 +8,6 @@ install-matlab-kernelspec
 
 echo "Installing Octave's control system toolbox"
 octave --eval "pkg install -forge control"
+
+echo "Adding utility functions to Octave's path"
+echo "addpath('book/res/matlab')" >> ~/.octaverc
